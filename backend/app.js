45b20eb5efc23bb730da17a7caa25c6458f17d34
veronnah <yoo.veroon@gmail.com>
2023-01-27 23:36:12 +0100

    fetching data from db

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://veronnah:FI7KrsGonX19BAel@cluster0.wd9gqay.mongodb.net/node-angular?retryWrites=true&w=majority")
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,30 +25,27 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+
   console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'sdaklfjs',
-      title: 'first post',
-      content: 'this is coming from the server',
-    },
-    {
-      id: 'sdaksderclfjs',
-      title: 'second post',
-      content: 'this is coming from the server',
-    },
-  ];
-  return res.status(200).json({
-    message: 'posts fetched successfully!',
-    posts: posts,
-  });
+  Post.find()
+    .then(posts => {
+      return res.status(200).json({
+        message: 'posts fetched successfully!',
+        posts: posts,
+      });
+    });
 });
 
 module.exports = app;
+
